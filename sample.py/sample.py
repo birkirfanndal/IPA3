20a346df5037c69400b247d28c241346f1dd8532
Birkir Fanndal <birkirfanndal1999@gmail.com>
2020-03-10 22:33:15 +0000

    added unittest

--- sample.py/sample.py
+++ sample.py/sample.py
@@ -1,10 +1,12 @@
 
-def FizzBuzz():
+def fizzbuzz(num):
     if num % 5 == 0 and num % 3 == 0:
         return "FizzBuzz"
     if num % 5 == 0:
         return "Buzz"
     if num % 3 == 0:
         return "Fizz"
+    else:
+        return num
    
     
--- sample.py/test_sample.py
+++ sample.py/test_sample.py
@@ -1,7 +1,28 @@
 import unittest
 from sample import fizzbuzz
 
-class SampleTest(unit.TestCase):
+class SampleTest(unittest.TestCase):
     def fizz_test(self):
         self.assertEqual(fizzbuzz(3), "Fizz")
-        
+        self.assertEqual(fizzbuzz(6), "Fizz")
+        self.assertEqual(fizzbuzz(9), "Fizz")
+        self.assertEqual(fizzbuzz(12), "Fizz")
+        self.assertEqual(fizzbuzz(18), "Fizz")
+
+        self.assertEqual(fizzbuzz(5), "Buzz")
+        self.assertEqual(fizzbuzz(10), "Buzz")
+        self.assertEqual(fizzbuzz(20), "Buzz")
+        self.assertEqual(fizzbuzz(35), "Buzz")
+        self.assertEqual(fizzbuzz(40), "Buzz")
+
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(fizzbuzz(30), "FizzBuzz")
+        self.assertEqual(fizzbuzz(45), "FizzBuzz")
+        self.assertEqual(fizzbuzz(60), "FizzBuzz")
+
+
+        self.assertEqual(fizzbuzz(2),2)
+    
+
+if __name__ == "__main__":
+    unittest.main()
